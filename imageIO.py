80ce0f91c01c971ad396f28418aaf455c8a413a6
rcamjalli <roni.camjalli@mercadolibre.com>
2018-12-26 21:32:00 -0300

    add log class

--- imageIO.py
+++ imageIO.py
@@ -1,22 +1,22 @@
 from scipy import misc
-import os, np
+import os, np, log
 def readImage(filename):
     if os.path.exists(filename):
-        print ("Log: reading from " + filename)
+        log.status("reading from " + filename)
         first = misc.imread(filename)
         first.tofile('.temp.raw')
         imageMemmap = np.memmap('.temp.raw', dtype=np.uint8, shape=first.shape)
-        print("Log: successfully read from" + filename)
+        log.status("successfully read from" + filename)
         try:
             os.remove(".temp.raw")
         finally:
             return imageMemmap
         
     else:
-        print("Error: can't read from " + filename)
+        log.error("can't read from " + filename)
         return None
 
 def writeImage(imageMemmap,filename):
-    print("Log: writing image to "+filename)
+    log.status("writing image to "+filename)
     misc.imsave(filename, imageMemmap)
-    print("Log: image successfully write to " + filename)
+    log.status("image successfully write to " + filename)
---
+++ log.py
@@ -0,0 +1,8 @@
+def status(text):
+    print("STATUS:"+text)
+def error(text):
+    print("ERROR:"+text)
+def info(text):
+    print("INFO:"+text)
+def warning(text):
+    print("WARNING:"+text)
